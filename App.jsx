3adf677dc6608e7f299db368a168095acabcde07
nishaj0 <njnishaj0@gmail.com>
2023-05-13 20:12:21 +0530

    removed unnecessary files

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+
+
+function App() {
+   return (
+     <BrowserRouter>
+       <h1>App</h1>
+     </BrowserRouter>
+   )
+ }
+
+export default App
